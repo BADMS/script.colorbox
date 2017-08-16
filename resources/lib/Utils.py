f38a48a1871bc16240af2926bebf3204cb8c8629
BADMS <dan@allskool.co.uk>
2017-08-16 18:46:08 +0100

    fixmain + tweeks (phew?)

--- resources/lib/Utils.py
+++ resources/lib/Utils.py
@@ -145,20 +145,20 @@ def Complementary_Color_Modify(im_color, com_color):
         hls = rgb_to_hls(int(irgb[0], 16)/255., int(irgb[1], 16)/255., int(irgb[2], 16)/255.)
         hls = hls_to_rgb(one_max_loop(hls[0]+color_hls[0]), one_max_loop(hls[1]+color_hls[1]), one_max_loop(hls[2]+color_hls[2]))
         return RGB_to_hex(hls)
-    elif color_comp == "fix" or color_comp == "fixboth":
+    elif color_comp == "fix":
         hls = rgb_to_hls(int(irgb[0], 16)/255., int(irgb[1], 16)/255., int(irgb[2], 16)/255.)
         hls = hls_to_rgb(one_max_loop(hls[0]+color_hls[0]), one_max_loop(color_hls[1]), one_max_loop(color_hls[2]))
         return RGB_to_hex(hls)
-    elif color_comp == "fixcomp":
+    elif color_comp == "fixcomp" or color_comp == "fixboth":
         hls = rgb_to_hls(int(crgb[0], 16)/255., int(crgb[1], 16)/255., int(crgb[2], 16)/255.)
-        hls = hls_to_rgb(hls[0], color_hls[1], color_hls[2])
+        hls = hls_to_rgb(one_max_loop(hls[0]+color_hls[0]), color_hls[1], color_hls[2])
         return RGB_to_hex(hls)
     return com_color
 def Image_Color_Modify(im_color):
     irgb = [im_color[2:4], im_color[4:6], im_color[6:8]]
-    if color_comp == "fixboth":
+    if color_comp == "fixboth" or color_comp == "fixmain":
         hls = rgb_to_hls(int(irgb[0], 16)/255., int(irgb[1], 16)/255., int(irgb[2], 16)/255.)
-        hls = hls_to_rgb(hls[0], one_max_loop(color_hls[1]), one_max_loop(color_hls[2]))
+        hls = hls_to_rgb(one_max_loop(hls[0]+color_hls[0]), one_max_loop(color_hls[1]), one_max_loop(color_hls[2]))
         return RGB_to_hex(hls)
     return im_color
 def Black_White(hex_color, prop):
